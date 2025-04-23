db424d48a29803135b9c3eb6e7016f6ed1b99b7b
ikh0120 <lkh50635164@gmail.com>
2025-04-23 11:15:42 +0900

    Account.java Add Method

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -4,4 +4,32 @@ public class Account {
 	private String custId;
 	private String acctId;
 	private int balance;
+	
+	
+	// 자동으로 Getter Setter 메서드 만들어줌
+	// Source/Generate Getters and Setters/
+	
+	// Setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	// Getter
+	public int getBalance() {
+		return balance;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	
 }
