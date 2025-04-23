f20bdfa134fd2a754763dabd06ecdd03398db5aa
ikh0120 <lkh50635164@gmail.com>
2025-04-23 10:52:18 +0900

    java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run ¥‹√‡≈∞: ctrl + f11 
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) { // main + <ctrl> + <space>
+		System.out.println("Hello Java"); // sysout + <ctrl> + <space>
+		
+	}
+}
