171efc20644c2429822979e00ea7eb14a39ea64f
ikh0120 <lkh50635164@gmail.com>
2025-04-23 14:46:15 +0900

    soource/Generate Getter and Setter

--- src/workshop/person/entity/PersonEntity.java
+++ src/workshop/person/entity/PersonEntity.java
@@ -17,5 +17,47 @@ public class PersonEntity {
 		this.address = address;
 		this.phone = phone;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
 	
 }
