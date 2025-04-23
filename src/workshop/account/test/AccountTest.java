7d5e826c1b2303d9aeaf0dfce8f0977e66c301cf
ikh0120 <lkh50635164@gmail.com>
2025-04-23 11:23:45 +0900

    밥 시간 전

--- src/workshop/account/test/AccountTest.java
+++ src/workshop/account/test/AccountTest.java
@@ -1,5 +1,12 @@
 package workshop.account.test;
 
-public class AccountTest {
+// ctrl + shift + o 누르면 instance 만들고 자동 import
+// 아니면 클래스 이름 적어넣고 ctrl + space
+import workshop.account.entity.Account;
 
+public class AccountTest {
+	public static void main(String[] args) {
+		// Account 객체 
+		Account account = new Account();
+	}
 }
