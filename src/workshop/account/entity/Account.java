403b7824937994290ce3b620a87f842c7ecdb8ba
ikh0120 <lkh50635164@gmail.com>
2025-04-23 13:27:21 +0900

    Constructor add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -5,10 +5,21 @@ public class Account {
 	private String acctId;
 	private int balance;
 	
+	//Default Constructor(기본 생성자)
+	public Account() {
+		System.out.println("Default Constructor Called..");
+	}
+	
+	//Constructor Overloading(생성자 중복 정의) 
+	// Source/Generate Constructor using Field...
+	public Account(String custId, String acctId, int balance) {
+		this.custId = custId;
+		this.acctId = acctId;
+		this.balance = balance;
+	}
 	
 	// 자동으로 Getter Setter 메서드 만들어줌
-	// Source/Generate Getters and Setters/
-	
+	// Source/Generate Getters and Setters
 	// Setter
 	public void setCustId(String custId) {
 		this.custId = custId;
--- src/workshop/account/test/AccountTest.java
+++ src/workshop/account/test/AccountTest.java
@@ -1,12 +1,11 @@
 package workshop.account.test;
 
-// ctrl + shift + o 누르면 instance 만들고 자동 import
-// 아니면 클래스 이름 적어넣고 ctrl + space
 import workshop.account.entity.Account;
 
 public class AccountTest {
 	public static void main(String[] args) {
 		// Account 객체 
-		Account account = new Account();
+		Account account = new Account("A1100", "221-22-3477", 100000);
+		System.out.println(account);
 	}
 }
