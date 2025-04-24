4763176ae0f3a27942b065e9405ab1e6405379cc
ikh0120 <lkh50635164@gmail.com>
2025-04-24 10:55:11 +0900

    Add workshop.animal.entity\Animal.java abstract/protected method study

--- src/workshop/animal/entity/Animal.java
+++ src/workshop/animal/entity/Animal.java
@@ -1,5 +1,17 @@
 package workshop.animal.entity;
 
-public class Animal {
-
+public abstract class Animal {
+	protected int legs;
+	
+	protected Animal(int legs) {
+		this.legs = legs;
+	}
+	
+	// 추상 메서드라 밑의 자식 클래스에서 만들거
+	public abstract void eat();
+	
+	public void walk() {
+		System.out.println("동물은 " + legs + "발로 걸어요. ");
+	}
+	
 }
